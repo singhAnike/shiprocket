efa714af4f4cc4c1c88c18fde4b9f2afc3deac69
Aniket <singh7509811123@gmail.com>
2025-04-05 15:40:48 +0530

    fixed minor

--- setup.py
+++ setup.py
@@ -4,26 +4,20 @@ with open('README.md') as readme:
     readme_content = readme.read()
 
 setup(
-    name="internal_pip_package",
+    name="Shiprocket",
     version="1.0.1",
-    description="This is the python client for the razorpayx and shiprocket",
+    description="This is the python client for the shiprocket",
     long_description=readme_content,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    author="Team Fatmug Design",
+    author="Aniket Singh",
     # license="MIT",
-    keywords="python client for the internal uses of the raxorpayx and shiprocket",
+    keywords="python client for the uses of the shiprocket",
     include_package_data=True,
-    # package_dir={'internal_pip_package': 'internal_pip_package', 'internal_pip_package.razorpayx':'internal_pip_package/razorpayx' ,'internal_pip_package.razorpayx.resources': 'internal_pip_package/razorpayx/resources', 'internal_pip_package.shiprocket': 'internal_pip_package/shiprocket' ,'internal_pip_package.shiprocket.resources': 'internal_pip_package/shiprocket/resources', 'internal_pip_package.razorpayx.constants':'internal_pip_package/razorpayx/constants'},
-    # packages=['internal_pip_package', 'internal_pip_package.razorpayx', 'internal_pip_package.razorpayx.resources',  'internal_pip_package.shiprocket', 'internal_pip_package.shiprocket.resources', 'internal_pip_package.razorpayx.constants'],
     install_requires=[
-    'razorpay==1.4.2',
     'cachetools==5.2.1',
     
 ],
-package_data={
-        'internal_pip_package.razorpayx': ['ca-bundle.crt'],  
-    }
 
    
 )
